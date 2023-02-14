918bf5a5919a1c906596ec3b2e229562dab3b22f
Winful KOfi Taylor <kofi.taylor07@gmail.com>
2023-02-14 02:51:44 +0000

    sucessfull get path to executable files

--- shell.c
+++ shell.c
@@ -1,7 +1,4 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#define DELIM " \n"
+#include "main.h"
 
 int main(void)
 {
@@ -9,35 +6,39 @@ int main(void)
   int num_of_tokens = 0, i;
   size_t n = 0;
   ssize_t no_chars_read;
-
   while(1)
     {
       printf("%s", prompt);
       no_chars_read = getline(&lineptr, &n, stdin);
-
       if(no_chars_read == -1)
 	{
 	  if (num_of_tokens == 0)
-	    free(lineptr);
-	    
-	  printf("Exiting shell...\n");
-	  return (-1);
+	    {
+	      free(lineptr);
+	      printf("Exiting shell...\n");
+	      return (-1);
+	    }
+	  else
+	    {
+	      for (i = 0; argv[i] != NULL; i++)
+		{
+		  free(argv[i]);
+		}
+	      free(argv);
+	      printf("Exiting shell...\n");
+	      return (-1);
+	    }
 	}
-
       if(num_of_tokens != 0)
 	num_of_tokens = 0;
-
       lineptr_cpy = malloc(sizeof(lineptr)*no_chars_read);
-
       if(lineptr_cpy == NULL)
 	{
 	  free(lineptr);
 	  perror("tsh:failure to allocate memory \n");
 	  return (-1);
 	}
-     
 	  strcpy(lineptr_cpy, lineptr);
-      
 	  token = strtok(lineptr_cpy, DELIM);
 	  while (token != NULL)
 	    {
@@ -45,17 +46,13 @@ int main(void)
 	      token = strtok(NULL, DELIM);
 	    }
 	  num_of_tokens++;
-      
-
       argv = malloc(sizeof(char *) * num_of_tokens);
       if(!argv)
 	{
 	  perror("lsh: failure to allocate memory\n");
 	  return (-1);
 	}
-    
 	  token = strtok(lineptr, DELIM);
-
       for (i = 0; token != NULL; i++)
 	{
 	  argv[i] = malloc(strlen(token) + 1);
@@ -63,18 +60,11 @@ int main(void)
 	  token = strtok(NULL, DELIM);
 	}
       argv[i] = NULL;
-
-
-      for (i = 0; argv[i] != NULL; i++)
-        {
-            free(argv[i]);
-        }
       
-      free(argv);
+      execmd(num_of_tokens, argv);
       free(lineptr_cpy);
       free(lineptr); 
     }
-
 
   return (0);
 }
